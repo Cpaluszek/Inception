03aafad6cfa557a445aa5ba4d13d82a979832e51
Clément Paluszek <105011036+Cpaluszek@users.noreply.github.com>
2023-04-19 13:23:58 +0200

    Update launch_wordpress.sh

--- srcs/requirements/wordpress/tools/launch_wordpress.sh
+++ srcs/requirements/wordpress/tools/launch_wordpress.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 
+# Automate the installation and configuration of wordpress on the server
+
+# Change directory to where the wordpress file will be downloaded
 cd /var/www/html/wordpress
 
-# DOWNLOAD WORDPRESS FILES
+# Download wordpress files
 wp core download  --path="/var/www/html/wordpress" --allow-root
 
-# GENERATE AND CONFIGURE THE WP-CONFIG.PHP FILE
+# Generate and configure wp-config.php - Including database connection info
 wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$DB_DATABASE --dbuser=$DB_USER --dbpass=$DB_USER_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
 
-# RUNS THE STANDARD WORDPRESS INSTALLATION PROCESS - CREATE THE WORDPRESS TABLES IN DB 
+# Install wordpress - configure URL / Title / Admin info
+# Also creates wordpress tables in the database
 wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_SITE_TITLE" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
 
-# CREATE USER IN WORDPRESS
 wp plugin update --allow-root --all
+
+# Create a new user
+
 wp user create --path="/var/www/html/wordpress" --allow-root $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD
 
-# GIVE PERMISSIONS TO GROUP AND USER WWW-DATA TO UPLOADS | -R EXECUTE ROOT
+# Permissions to WWW-DATA for the upload directory | -R EXECUTE ROOT
 chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
 
 # LAUNCH PHP-FPM
